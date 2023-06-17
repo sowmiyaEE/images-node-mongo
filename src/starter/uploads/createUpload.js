449b14bde857af6817e4a8119658d1d9a70e01f1
sowmiya E <sowmiya.skm@gmail.com>
2023-06-17 12:32:22 +0530

    remove console.log

--- src/starter/uploads/createUpload.js
+++ src/starter/uploads/createUpload.js
@@ -1,6 +1,5 @@
 const config = require("config");
 const {ObjectId} = require("mongodb");
-var objectId = new ObjectId();
 
 const uploadFile = (request, response) => {
   const file = request.file;
@@ -21,24 +20,32 @@ const getFile = (request, response) => {
       msg: "id param required"
     })
   }
-  const images = DATABASE.collection('photos.files');
-  const chunks = DATABASE.collection('photos.chunks');
+  const images = DATABASE.collection('photos.files').find({ _id: new ObjectId(requestFileId)});
+  const chunks = DATABASE.collection('photos.chunks').find({ files_id: new ObjectId(requestFileId)} );
   let contentType='';
-  images.find({ _id: new ObjectId(requestFileId)}).toArray().then( data =>
-   { 
-   if(!data.length){ throw new Error('data not found');}
-   contentType = data[0].contentType; return data; }
-  ).then(data => 
-     chunks.find( {files_id: new ObjectId(data[0]._id)} ).sort({n:1}).toArray())
-   .then(ordered => {
+  images
+  .toArray()
+  .then( data => { 
+    if(!data.length){ 
+      throw new Error('Image id not found');
+    }
+    contentType = data[0].contentType;
+    return data[0]; 
+  })
+  .then(data => 
+     chunks
+     .sort({n:1})
+     .toArray())
+  .then(ordered => {
     let url = `data:${contentType};base64,`;
     for( const chunk of ordered) { 
       let base64 = chunk.data.toString('base64');
       url += base64;
     }
     return response.status(200).send({url:url});
-  }).catch(error => {
-   return response.status(400).send({
+  })
+  .catch(error => {
+    return response.status(400).send({
      success: false,
      msg: error.message
    })
